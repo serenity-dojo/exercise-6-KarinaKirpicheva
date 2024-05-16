4d22df992ead35433efe9e4d149e0e30f4d75e26
Karina Kirpicheva <karina@sayprimer.com>
2024-05-16 23:11:48 +0400

    Exercises

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -1,7 +1,20 @@
 package com.serenitydojo.model;
 
 public class Feeder {
-    public String feeds(String animal, boolean isPremium) {
-        return "Food";
+    public FoodType feeds(String animal, boolean isPremium) {
+
+        if (animal.equals("Cat")) {
+            return (isPremium) ? FoodType.SALMON : FoodType.TUNA;
+            } else if (animal.equals("Dog")) {
+            return (isPremium) ? FoodType.DELUXE_DOG_FOOD : FoodType.DOG_FOOD;
+            } else if (animal.equals("Hamster")) {
+            return (isPremium) ? FoodType.LETTUCE : FoodType.CABBAGE;
+            }
+                return FoodType.UNKNOWN;
+
     }
+
 }
+
+
+
